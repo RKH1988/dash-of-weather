826e8e0722eb4ac0741e59cc6980cc56d34ec73e
RKH1988 <regettaspearman@gmail.com>
2022-07-14 16:17:57 -0400

    added forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,31 @@ var currTempEl=document.querySelector("#currTemp");
 var currWindEl=document.querySelector("#currWind");
 var currHumidityEl=document.querySelector("#currHumidity");
 var currUvEl=document.querySelector("#currUv");
+var date1El=document.querySelector("#date1");
+var date2El=document.querySelector("#date2");
+var date3El=document.querySelector("#date3");
+var date4El=document.querySelector("#date4");
+var date5El=document.querySelector("#date5");
+var img1El=document.querySelector("#img1");
+var img2El=document.querySelector("#img2");
+var img3El=document.querySelector("#img3");
+var img4El=document.querySelector("#img4");
+var img5El=document.querySelector("#img5");
+var temp1El=document.querySelector("#Temp1");
+var temp2El=document.querySelector("#Temp2");
+var temp3El=document.querySelector("#Temp3");
+var temp4El=document.querySelector("#Temp4");
+var temp5El=document.querySelector("#Temp5");
+var wind1El=document.querySelector("#Wind1");
+var wind2El=document.querySelector("#Wind2");
+var wind3El=document.querySelector("#Wind3");
+var wind4El=document.querySelector("#Wind4");
+var wind5El=document.querySelector("#Wind5");
+var humidity1El=document.querySelector("#Humidity1");
+var humidity2El=document.querySelector("#Humidity2");
+var humidity3El=document.querySelector("#Humidity3");
+var humidity4El=document.querySelector("#Humidity4");
+var humidity5El=document.querySelector("#Humidity5");
 
 var formSubmitHandler = function(event) {
     //prevent page from refreshing
@@ -30,10 +55,7 @@ var formSubmitHandler = function(event) {
 
     if (city) {
         getCoordinates(city);
-
-        //clear old content
-        //currentContainerEl.textContent="";
-        cityInputEl.value="";
+        //localStorage.setItem("cityList", city);
     } else {
         alert("Please enter a city name");
     }
@@ -76,6 +98,7 @@ var getCurrWeather = function(lat,lon){
             response.json().then(function(data){
                 console.log(data);
                 displayCurrWeather(lat,lon);
+                displayForecast(lat,lon);
             });
         } else {
             alert("Error: City Not Found");
@@ -91,7 +114,7 @@ var displayCurrWeather = function(lat, lon) {
             console.log(response);
             response.json().then(function(data){
                 console.log(data)
-                currDateEl.textContent="  "+ moment(current.dt).format("M/D/YYYY");
+                currDateEl.textContent="  ("+ moment(current.dt).format("M/D/YYYY")+")";
                 var currImgEl=document.querySelector("#currImg");
                 var icon=data.current.weather[0].icon;
                 currImgEl.setAttribute("src",iconStartUrl+icon+iconEndUrl);
@@ -130,6 +153,50 @@ var editUvIndex = function(lat,lon) {
         }    
     });
 };
+
+var displayForecast = function(lat,lon) {
+    var apiUrl=apiBegin +lat +"&lon="+lon+apiEnd;
+    fetch(apiUrl).then(function(response){
+        if(response.ok) {
+            console.log(response);
+            response.json().then(function(data){
+                console.log(data)
+                //display dates
+                date1El.textContent= moment(current.dt).add(1,"days").format("M/D/YYYY");
+                date2El.textContent=moment(current.dt).add(2,"days").format("M/D/YYYY");
+                date3El.textContent=moment(current.dt).add(3,"days").format("M/D/YYYY");
+                date4El.textContent=moment(current.dt).add(4,"days").format("M/D/YYYY");
+                date5El.textContent=moment(current.dt).add(5,"days").format("M/D/YYYY");
+                img1El.setAttribute("src",iconStartUrl+data.daily[0].weather[0].icon+iconEndUrl);
+                img2El.setAttribute("src",iconStartUrl+data.daily[1].weather[0].icon+iconEndUrl);
+                img3El.setAttribute("src",iconStartUrl+data.daily[2].weather[0].icon+iconEndUrl);
+                img4El.setAttribute("src",iconStartUrl+data.daily[3].weather[0].icon+iconEndUrl);
+                img5El.setAttribute("src",iconStartUrl+data.daily[4].weather[0].icon+iconEndUrl);
+                temp1El.textContent=data.daily[0].temp.max+"°F"
+                temp2El.textContent=data.daily[1].temp.max+"°F"
+                temp3El.textContent=data.daily[2].temp.max+"°F"
+                temp4El.textContent=data.daily[3].temp.max+"°F"
+                temp5El.textContent=data.daily[4].temp.max+"°F"
+                wind1El.textContent=data.daily[0].wind_speed+" MPH";
+                wind2El.textContent=data.daily[1].wind_speed+" MPH";
+                wind3El.textContent=data.daily[2].wind_speed+" MPH";
+                wind4El.textContent=data.daily[3].wind_speed+" MPH";
+                wind5El.textContent=data.daily[4].wind_speed+" MPH";
+                humidity1El.textContent=data.daily[0].humidity+"%";
+                humidity2El.textContent=data.daily[1].humidity+"%";
+                humidity3El.textContent=data.daily[2].humidity+"%";
+                humidity4El.textContent=data.daily[3].humidity+"%";
+                humidity5El.textContent=data.daily[4].humidity+"%";
+            });           
+        } else {
+            alert("Error: City Not Found");
+        }    
+    });
+};
+
+//save to local storage
+
+
 
 //convert Unix Timestamp to date
  //  moment(dt).format("M/D/YYYY");
